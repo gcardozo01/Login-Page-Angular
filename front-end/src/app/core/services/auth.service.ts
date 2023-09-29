f38a0b64805e0366115ccb8e4d05aa966c523ed5
Guilherme Cardozo <guilhermecardozo91@gmail.com>
2023-09-29 12:31:56 -0300

    Initial commit

---
+++ front-end/src/app/core/services/auth.service.ts
@@ -0,0 +1,50 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { Observable, catchError, map, throwError } from 'rxjs';
+
+// Services
+import { JwtHelperService } from '@auth0/angular-jwt';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  private url: string = 'http://localhost:3000';
+
+  constructor(private Http: HttpClient, private router: Router) {}
+
+  public sign(payload: { email: string; password: string }): Observable<any> {
+    return this.Http.post<{ token: string }>(`${this.url}/sign`, payload).pipe(
+      map((res) => {
+        localStorage.removeItem('access_token');
+        localStorage.setItem('access_token', res.token);
+        return this.router.navigate(['admin']);
+      }),
+      catchError((e) => {
+        if (e.error.message) return throwError(() => e.error.message);
+
+        return throwError(
+          () =>
+            'No momento não estamos conseguindo validar este dado. Tente novamente mais tarde!'
+        );
+      })
+    );
+  }
+
+  public logout() {
+    localStorage.removeItem('access_token');
+    return this.router.navigate(['']);
+  }
+
+  public isAuthenticated(): boolean {
+    const token = localStorage.getItem('access_token');
+
+    if (!token) {
+      return false;
+    }
+    const jwtHelper = new JwtHelperService();
+
+    return !jwtHelper.isTokenExpired(token);
+  }
+}
